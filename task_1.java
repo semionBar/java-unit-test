6bdc254a1896e5c04a08765017bc93b5b54d5a63
Semen Baranov <b4ranov.s.a@yandex.com>
2024-04-28 17:10:46 +0300

    Added code to 1 and 2 task

---
+++ task_1.java
@@ -0,0 +1,6 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	assertEquals("Тест провален",true, isAdult);
+}
